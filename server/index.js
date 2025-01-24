c681fe0abdf827acacb83c1d2a20bb0ae1c5f30a
Matt Henderson <mattcharlesh@gmail.com>
2025-01-24 12:05:20 -0500

    Updated CORS origin env variable and cleanup

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_BASE_URL || 'http://localhost:3002',
   credentials: true
 }));
 app.use(express.json());
